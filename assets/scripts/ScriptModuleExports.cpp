d9c62e3c33d87e415ed9bc2c6e48db15a28a101a
Liu Sizhe <39219648+JamesLiu12@users.noreply.github.com>
2026-03-03 23:05:59 +0800

    refactor: change GAME_EXPORT to PROJECT_EXPORT

--- assets/scripts/ScriptModuleExports.cpp
+++ assets/scripts/ScriptModuleExports.cpp
@@ -1,17 +1,17 @@
 #include <Ignis/Script/ScriptAutoRegistry.h>
 
 #if defined(_WIN32)
-#define GAME_EXPORT extern "C" __declspec(dllexport)
+#define PROJECT_EXPORT extern "C" __declspec(dllexport)
 #else
-#define GAME_EXPORT extern "C" __attribute__((visibility("default")))
+#define PROJECT_EXPORT extern "C" __attribute__((visibility("default")))
 #endif
 
-GAME_EXPORT void RegisterProjectScripts(ignis::ScriptRegistry& registry)
+PROJECT_EXPORT void RegisterProjectScripts(ignis::ScriptRegistry& registry)
 {
 	ignis::RegisterPendingScripts(registry);
 }
 
-GAME_EXPORT void UnregisterProjectScripts(ignis::ScriptRegistry& registry)
+PROJECT_EXPORT void UnregisterProjectScripts(ignis::ScriptRegistry& registry)
 {
 	ignis::UnregisterPendingScripts(registry);
 }
